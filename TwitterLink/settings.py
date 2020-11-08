5f7a630f2dc6d72cf9ba03ced7746882b53018d4
yashacon <17dcs017@lnmiit.ac.in>
2020-11-09 01:22:52 +0530

    static folder

--- TwitterLink/settings.py
+++ TwitterLink/settings.py
@@ -14,6 +14,8 @@ from decouple import config
 from pathlib import Path
 import dj_database_url
 import mimetypes
+
+
 mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'app/static/'),
